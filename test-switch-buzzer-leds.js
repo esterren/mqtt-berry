ead0b35bd009a2bb8c8c7e8f0ca3c12315ae082b
esterren <renato.estermann@gmail.com>
2014-05-04 17:41:32 +0200

    added testscript for buzzer an button an integrated into app.js

--- test-switch-buzzer-leds.js
+++ test-switch-buzzer-leds.js
@@ -1,46 +1,21 @@
-var mqtt = require("mqtt"),
-    Gpio = require("onoff").Gpio,
+/**
+ * Created by rest on 04.05.14.
+ */
+var Gpio = require("onoff").Gpio,
     sh = require('execSync'),
     config = require("./config.json");
-//    leds = require("./leds.js");
 
 var onoffStates= {
-        ON: {'value':1,'description':'ON'},
-        OFF:{'value':0,'description':'OFF'}
+    ON: {'value':1,'description':'ON'},
+    OFF:{'value':0,'description':'OFF'}
 }
 
 var ledsA = [],
     buzzerA = [],
     buttonA = [];
 
-var basetopic = config.mqtt.options.clientId +'/berryclip/',
-    ledtopic = basetopic+'leds/',
-    buzzertopic = basetopic+'buzzers/',
-    buttontopic = basetopic+'buttons/',
-    ledstatuspattern = new RegExp("^"+ledtopic.replace('/',"\\/")+'[0-9]+\/status',"g"),
-    buzzerstatuspattern = new RegExp("^"+buzzertopic.replace('/',"\\/")+'[0-9]+\/status',"g"),
-    buttonstatuspattern = new RegExp("^"+buttontopic.replace('/',"\\/")+'[0-9]+\/status',"g");
-
-client = mqtt.createClient(config.mqtt.port, config.mqtt.host, config.mqtt.options);
-client.subscribe(basetopic+'/#');
-
 (function initGPIOs(){
     exportGPIOs();
-//    if(config.berryclip.leds){
-//        config.berryclip.leds.forEach(function(obj){
-//            var ledObject = new Gpio(obj.gpio_nr,obj.direction);
-//            obj.ledObject = ledObject;
-//            obj.status = onoffStates.ON;
-//            console.log(obj);
-//            ledsA.push(obj);
-//            ledObject.writeSync(obj.status.value);
-//            client.publish(ledtopic+obj.id+'/color', obj.color);
-//            client.publish(ledtopic+obj.id+'/description', obj.description);
-//            client.publish(ledtopic+obj.id+'/status', obj.status.description);
-//        });
-//
-//    }
-
     if(config.berryclip.leds){
         config.berryclip.leds.forEach(function(obj){
             var ledObject = new Gpio(obj.gpio_nr,obj.direction);
@@ -49,9 +24,6 @@ client.subscribe(basetopic+'/#');
             console.log(obj);
             ledsA.push(obj);
             ledObject.writeSync(obj.status.value);
-            client.publish(ledtopic+obj.id+'/color', obj.color);
-            client.publish(ledtopic+obj.id+'/description', obj.description);
-            client.publish(ledtopic+obj.id+'/status', obj.status.description);
         });
 
     }
@@ -64,8 +36,6 @@ client.subscribe(basetopic+'/#');
             console.log(obj);
             buzzerA.push(obj);
             buzObject.writeSync(obj.status.value);
-            client.publish(buzzertopic+obj.id+'/description', obj.description);
-            client.publish(buzzertopic+obj.id+'/status', obj.status.description);
         });
 
     }
@@ -92,43 +62,6 @@ client.subscribe(basetopic+'/#');
 })();
 
 
-
-client.on('message', function (topic, message) {
-
-    console.log(topic, message);
-    if(ledstatuspattern.test(topic)){
-        var ledid = parseInt(topic.replace(ledtopic,'').match(/[0-9]+/g)[0]);
-        console.log(ledid);
-
-        tlid = arrayObjectIndexOf(ledsA,ledid,'id');
-        console.log(tlid);
-        if(tlid>=0){
-            if(onoffStates[message.toUpperCase()]){
-
-                ledsA[tlid].ledObject.write(onoffStates[message.toUpperCase()].value, function(err) { // Asynchronous write.
-                    if (err) throw err;
-                    //ledsA[tlid].status = onoffStates.ON;
-                });
-            }
-        }
-    }
-});
-
-
-//client.end();
-
-
-function arrayObjectIndexOf(myArray, searchTerm, property) {
-    for(var i = 0, len = myArray.length; i < len; i++) {
-        if (myArray[i][property] === searchTerm) return i;
-    }
-    return -1;
-}
-
-/*
-    This function exports all configured GPIOs,
-    because superuser privileges are required for exporting and using GPIOs on the RPi.
- */
 function exportGPIOs(){
     if(config.berryclip.leds){
         config.berryclip.leds.forEach(function(obj){
@@ -153,9 +86,6 @@ function exportGPIOs(){
     }
 }
 
-/*
-    This function sets the IO-value to 0 and unexports the GPIOs
- */
 function unexportGPIOs(){
     if(config.berryclip.leds){
         config.berryclip.leds.forEach(function(obj){
@@ -177,10 +107,7 @@ function exitHandler(options, err) {
     if (options.cleanup){
         console.log(' Exiting...');
         unexportGPIOs();
-//        ledsA.forEach(function (led) {
-//            led.ledObject.writeSync(0);
-//            led.ledObject.unexport();
-//        });
+
     }
     if (err) console.log(err.stack);
     if (options.exit) process.exit();
